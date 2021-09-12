0e0fcfddac5ca07476f720843575f81ddae1997d
Ayush Rameja <67544555+RamejaAyush@users.noreply.github.com>
2021-09-12 20:14:05 +0530

    added nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { VscGithubAlt } from "react-icons/vsc";
 import { FiTwitter } from "react-icons/fi";
 import { RiLinkedinFill } from "react-icons/ri";
 import { motion } from "framer-motion";
+import Nav from "./Nav";
 
 export default function Header() {
  const headerAnim = {
@@ -18,35 +19,56 @@ export default function Header() {
   },
  };
 
+ const imgAnim = {
+  hidden: {
+   opacity: 0,
+   scale: 1.5,
+  },
+  show: {
+   opacity: 1,
+   scale: 1,
+   transition: { duration: 1 },
+  },
+ };
+
  return (
-  <motion.header>
-   <motion.div
-    variants={headerAnim}
-    initial="hidden"
-    animate="show"
-    className="pro-container"
-   >
-    <div className="image">
-     <img src={profileImg} alt="ayush Rameja" />
-    </div>
-    <div className="name">
-     <h1>Ayush Rameja</h1>
-    </div>
-    <div className="desc">
-     <p>Front End Developer</p>
-    </div>
-    <div className="social-links">
-     <a href="sad">
-      <VscGithubAlt title="Github" />
-     </a>
-     <a href="sad">
-      <FiTwitter title="Twitter" />
-     </a>
-     <a href="sad">
-      <RiLinkedinFill title="LinkedIn" />
-     </a>
-    </div>
-   </motion.div>
-  </motion.header>
+  <>
+   <motion.header>
+    <motion.div
+     variants={headerAnim}
+     initial="hidden"
+     animate="show"
+     className="pro-container"
+    >
+     <div className="image">
+      <motion.img
+       variants={imgAnim}
+       initial="hidden"
+       animate="show"
+       src={profileImg}
+       alt="ayush Rameja"
+      />
+     </div>
+     <div className="name">
+      <h1>Ayush Rameja</h1>
+     </div>
+     <div className="desc">
+      <p>Front End Developer</p>
+     </div>
+     <div className="social-links">
+      <a href="sad">
+       <VscGithubAlt title="Github" />
+      </a>
+      <a href="sad">
+       <FiTwitter title="Twitter" />
+      </a>
+      <a href="sad">
+       <RiLinkedinFill title="LinkedIn" />
+      </a>
+     </div>
+    </motion.div>
+   </motion.header>
+   <Nav />
+  </>
  );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import "../styles/Nav.css";
+
+const Nav = () => {
+ return (
+  <nav>
+   <a href="asd">About</a>
+   <a href="asd">Projects</a>
+   <a href="asd">Contact</a>
+  </nav>
+ );
+};
+
+export default Nav;
