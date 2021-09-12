25eb60518bea91f3371582a5ee584a4d5aaaafa2
Ayush Rameja <67544555+RamejaAyush@users.noreply.github.com>
2021-09-12 20:00:56 +0530

    header section updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,52 @@
+import "../styles/Header.css";
+import profileImg from "../media/profile.jpg";
+import { VscGithubAlt } from "react-icons/vsc";
+import { FiTwitter } from "react-icons/fi";
+import { RiLinkedinFill } from "react-icons/ri";
+import { motion } from "framer-motion";
+
 export default function Header() {
+ const headerAnim = {
+  hidden: {
+   opacity: 0,
+   y: 50,
+  },
+  show: {
+   opacity: 1,
+   y: 0,
+   transition: { duration: 1, ease: "easeOut" },
+  },
+ };
+
  return (
-  <header>
-   <div className="image">
-    <img src="#" alt="ayush Rameja" />
-   </div>
-  </header>
+  <motion.header>
+   <motion.div
+    variants={headerAnim}
+    initial="hidden"
+    animate="show"
+    className="pro-container"
+   >
+    <div className="image">
+     <img src={profileImg} alt="ayush Rameja" />
+    </div>
+    <div className="name">
+     <h1>Ayush Rameja</h1>
+    </div>
+    <div className="desc">
+     <p>Front End Developer</p>
+    </div>
+    <div className="social-links">
+     <a href="sad">
+      <VscGithubAlt title="Github" />
+     </a>
+     <a href="sad">
+      <FiTwitter title="Twitter" />
+     </a>
+     <a href="sad">
+      <RiLinkedinFill title="LinkedIn" />
+     </a>
+    </div>
+   </motion.div>
+  </motion.header>
  );
 }
